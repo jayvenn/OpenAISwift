c3db3ed9e5bf55a545581b1dfa9271083d24a628
Jayven Nhan <jayvennhan@gmail.com>
2024-12-26 15:38:22 -0500

    Initial Commit

---
+++ OpenAISwift/OpenAISwift.h
@@ -0,0 +1,18 @@
+//
+//  OpenAISwift.h
+//  OpenAISwift
+//
+//  Created by Jayven on 26/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenAISwift.
+FOUNDATION_EXPORT double OpenAISwiftVersionNumber;
+
+//! Project version string for OpenAISwift.
+FOUNDATION_EXPORT const unsigned char OpenAISwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenAISwift/PublicHeader.h>
+
+
---
+++ OpenAISwiftTests/OpenAISwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAISwiftTests.swift
+//  OpenAISwiftTests
+//
+//  Created by Jayven on 26/12/24.
+//
+
+import Testing
+@testable import OpenAISwift
+
+struct OpenAISwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
